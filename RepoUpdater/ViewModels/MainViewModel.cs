3a1b3bf1db6a3daa66fd003ef0d5833b03f5be88
Przemyslaw Liszka <mistix@op.pl>
2015-02-24 21:38:31 +0100

    Implement MVVM pattern. Create commands for exit main window and for open new item window.

--- RepoUpdater/ViewModels/MainViewModel.cs
+++ RepoUpdater/ViewModels/MainViewModel.cs
@@ -3,25 +3,45 @@ using RepoUpdater.Model.Factories;
 using RepoUpdater.Model.ModelView;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Windows.Input;
 using TinyMessenger;
 
 namespace RepoUpdater.ViewModels
 {
-    public class MainViewModel : IMainViewModel
+    public class MainViewModel : IMainViewModel, INotifyPropertyChanged, IDisposable
     {
+        #region Fields
+
         private readonly IKernel _kernel;
         private readonly ITinyMessengerHub _messageBus;
         private readonly IRepositoryFactory _repositoryFactory;
         private readonly INavigationManager _navigationManager;
 
         private RelayCommand _closeMainWindow;
+        private RelayCommand _openNewItemWindow;
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler CloseWindow;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Properties
 
         public IEnumerable<RepositoryItem> Repositories { get; set; }
 
         public ICommand OpenNewItemWindow
         {
-            get { return null; }
+            get
+            {
+                return _openNewItemWindow ??
+                       (_openNewItemWindow = new RelayCommand(param => _navigationManager.OpenAddNewItemWindow()));
+            }
         }
 
         public ICommand CloseMainWindow
@@ -32,14 +52,46 @@ namespace RepoUpdater.ViewModels
             }
         }
 
-        public event EventHandler CloseWindow;
+        #endregion
 
-        public MainViewModel(IKernel kernel, ITinyMessengerHub messageBus, IRepositoryFactory repositoryFactory, INavigationManager navigationManager)
+        #region Constructors
+
+        public MainViewModel(IKernel kernel, ITinyMessengerHub messageBus, IRepositoryFactory repositoryFactory,
+            INavigationManager navigationManager)
         {
             _kernel = kernel;
             _messageBus = messageBus;
             _repositoryFactory = repositoryFactory;
             _navigationManager = navigationManager;
         }
+
+        #endregion
+
+        #region Methods
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler == null) return;
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] != null) return;
+            string msg = "Invalid property name: " + propertyName;
+            throw new Exception(msg);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
     }
 }
